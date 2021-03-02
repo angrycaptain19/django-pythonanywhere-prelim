3d9ec55b81ffe8ffb30e3e8f299b69a088d0683b
Sourcery AI <>
2021-03-02 09:19:22 +0000

    'Refactored by Sourcery'

--- todo/views.py
+++ todo/views.py
@@ -39,14 +39,12 @@ def signupuser(request):
 def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', { 'form': AuthenticationForm()})
-    else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
 
-        if user is None:
-            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password are incorrect'})
-        else:
-            login(request, user)
-            return redirect('currenttodos')
+    if user is None:
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password are incorrect'})
+    login(request, user)
+    return redirect('currenttodos')
 
 @login_required
 def logoutuser(request):
